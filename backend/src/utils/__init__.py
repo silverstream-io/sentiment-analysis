c544c0bcb972ca1e6fd730522c339362978024c3
Cory Waddingham <cory@waddingham.org>
2024-10-17 09:54:52 -0700

    Corrected circular import of logging.

--- backend/src/utils/__init__.py
+++ backend/src/utils/__init__.py
@@ -4,17 +4,15 @@ import os, datetime
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
-def setup_logging():
-    # Set up logging
-    log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '../logs')
-    os.makedirs(log_dir, exist_ok=True)
-    log_file = os.path.join(log_dir, f"{datetime.now().strftime('%Y-%m-%d')}.log")
-    logger = logging.getLogger('api.server')
-    logger.setLevel(logging.DEBUG)
-    file_handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, backupCount=30)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-    logger.addHandler(file_handler)
-    return logger
+# Set up logging
+log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '../logs')
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, f"{datetime.now().strftime('%Y-%m-%d')}.log")
+logger = logging.getLogger('api.server')
+logger.setLevel(logging.DEBUG)
+file_handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, backupCount=30)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
 
 def get_subdomain(request: Request) -> Tuple[Optional[str], Optional[Tuple[Dict[str, str], int]]]:
     """
