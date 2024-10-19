2932fd146f659401e841c94784edf2ad7b5d5007
Cory Waddingham <cory@waddingham.org>
2024-10-18 21:51:50 -0700

    Updated app to use cookies to manage authn after the initial JWT token is sent (JWT is only sent in the first request and may not be present in subsequent, requiring cookies to manage authn after).

--- backend/src/api/server.py
+++ backend/src/api/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_session import Session
 from flask_cors import CORS
 import dotenv
 import logging
@@ -9,6 +10,7 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.register_blueprint(api_blueprint)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+Session(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
