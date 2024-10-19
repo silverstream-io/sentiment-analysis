42ccc26296e6e7e75d16db69fd7cc792818f8283
Cory Waddingham <cory@waddingham.org>
2024-10-19 13:45:05 -0700

    Prettified the 404 page.

--- backend/src/api/server.py
+++ backend/src/api/server.py
@@ -19,7 +19,7 @@ Session(app)
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
