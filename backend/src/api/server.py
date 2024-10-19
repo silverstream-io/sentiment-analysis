e2ac0a5e732f610c1c749b2f26be5172461d6a60
Cory Waddingham <cory@waddingham.org>
2024-10-19 12:55:39 -0700

    Fixed paths of template folders and added a custom 404.

--- backend/src/api/server.py
+++ backend/src/api/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_session import Session
 from flask_cors import CORS
 import dotenv
@@ -9,14 +9,17 @@ from .routes import root as root_blueprint, sentiment_checker as sentiment_check
 dotenv.load_dotenv()
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
+app.template_folder = '../templates'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.register_blueprint(root_blueprint)
-root_blueprint.template_folder = '../templates/root'
-app.register_blueprint(sentiment_checker_blueprint)
-sentiment_checker_blueprint.template_folder = '../templates/sentiment-checker'
+app.register_blueprint(root_blueprint, template_folder='../templates/root')
+app.register_blueprint(sentiment_checker_blueprint, template_folder='../templates/sentiment-checker')
 CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
 Session(app)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
