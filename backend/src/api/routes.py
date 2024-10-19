5b085539dfcb3216adf041e762d968c6bced72c0
Cory Waddingham <cory@waddingham.org>
2024-10-19 10:10:36 -0700

    Changed API routes to be for sentiment-checker specifically, to allow for additional routes for other apps in the future.

--- backend/src/api/routes.py
+++ backend/src/api/routes.py
@@ -2,13 +2,16 @@ from flask import Blueprint
 from .views import index, health, analyze_comments, get_score, get_ticket_vectors, entry
 import logging
 
-api = Blueprint('api', __name__)
 logger = logging.getLogger('api.server')
-logger.debug("Initializing API routes")
+logger.debug("Initializing routes")
 
-api.add_url_rule('/', 'index', index)
-api.add_url_rule('/sentiment-checker/health', 'health', health, methods=['GET'])
-api.add_url_rule('/sentiment-checker/analyze-comments', 'analyze_comments', analyze_comments, methods=['POST'])
-api.add_url_rule('/sentiment-checker/get-score', 'get_score', get_score, methods=['POST'])
-api.add_url_rule('/sentiment-checker/get-ticket-vectors', 'get_ticket_vectors', get_ticket_vectors, methods=['POST'])
-api.add_url_rule('/sentiment-checker/entry', 'entry', entry, methods=['POST'])
+root = Blueprint('root', __name__)
+root.add_url_rule('/', 'index', index)
+
+logger.debug("Initializing sentiment-checker routes")
+sentiment_checker = Blueprint('sentiment-checker', __name__, url_prefix='/sentiment-checker')
+sentiment_checker.add_url_rule('/health', 'health', health, methods=['GET'])
+sentiment_checker.add_url_rule('/analyze-comments', 'analyze_comments', analyze_comments, methods=['POST'])
+sentiment_checker.add_url_rule('/get-score', 'get_score', get_score, methods=['POST'])
+sentiment_checker.add_url_rule('/get-ticket-vectors', 'get_ticket_vectors', get_ticket_vectors, methods=['POST'])
+sentiment_checker.add_url_rule('/', 'entry', entry, methods=['POST'])
