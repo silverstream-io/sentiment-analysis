abc8a007cf5b97b8f7c92a5d693673dc170afe17
Cory Waddingham <cory@waddingham.org>
2024-10-19 12:00:13 -0700

    Split up templates into their own class directories, ensured the template_folder is set on each blueprint.

--- backend/src/config-template.py
+++
@@ -1,6 +0,0 @@
-ZENDESK_API_TOKEN = 'your_zendesk_api_token'
-ZENDESK_EMAIL = 'your_zendesk_email'
-ZENDESK_SUBDOMAIN = 'your_zendesk_subdomain'
-OPENAI_API_KEY = 'your_openai_api_key'
-DATABASE_URL = 'your_database_url'
-JWT_SECRET = 'your_jwt_secret_key'
