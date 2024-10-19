d3b7021cbccf68b83222a19120970bd9c8583b1e
Cory Waddingham <cory@waddingham.org>
2024-10-18 22:10:00 -0700

    Added session management to the backend via Flask sessions.

--- backend/src/api/server.py
+++ backend/src/api/server.py
@@ -3,11 +3,14 @@ from flask_session import Session
 from flask_cors import CORS
 import dotenv
 import logging
+import os
 from .routes import api as api_blueprint
 
 dotenv.load_dotenv()
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.register_blueprint(api_blueprint)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 Session(app)
