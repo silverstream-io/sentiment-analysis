4c3657fb738e4a8780ed4e9394f7c3a8cfb0a4e0
Cory Waddingham <cory@waddingham.org>
2024-10-23 21:13:40 -0700

    Increased debug logging for background service.

--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -341,13 +341,18 @@ class SentimentChecker:
     def background_refresh(self):
         if request.method == 'POST':
             if request.is_json:
+                self.logger.debug(f"Received JSON data for background_refresh, request remote addr: {self.remote_addr}")
                 data = request.get_json()
             elif request.form:
+                self.logger.debug(f"Received form data for background_refresh, request remote addr: {self.remote_addr}")
                 data = request.form.to_dict()
             elif request.data:
+                self.logger.debug(f"Received data for background_refresh, request remote addr: {self.remote_addr}")
                 data = request.data.decode('utf-8')
             else:
+                self.logger.warning(f"Received unknown data for background_refresh, request remote addr: {self.remote_addr}")
                 data = None
+        self.logger.debug(f"Received data {data} for background_refresh, request remote addr: {self.remote_addr}")
         
         original_query_string = request.query_string.decode()   
         response = make_response(render_template(
