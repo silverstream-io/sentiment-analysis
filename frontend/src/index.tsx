3982844b8e29dbaf31b2a3d37b2cfa2c85f3a09f
Cory Waddingham <cory@waddingham.org>
2024-10-24 13:44:07 -0700

    Changed Route to use HashRoute so that it works within a nested app environment.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './components/App';
 import TopbarApp from './components/TopbarApp';
 import BackgroundApp from './components/BackgroundApp';
