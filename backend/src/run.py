29c29164279fc22bc14dbec5ca5e7964728523ae
Cory Waddingham <cory@waddingham.org>
2024-10-24 15:22:54 -0700

    Corrected bug in how the Flask regex converted was being constructed.

--- backend/src/run.py
+++ backend/src/run.py
@@ -1,5 +1,12 @@
 from api.server import create_app
+from werkzeug.routing import BaseConverter
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
 
 if __name__ == '__main__':
     app = create_app()
+    app.url_map.converters['regex'] = RegexConverter
     app.run(host='0.0.0.0', port=4000, debug=True)
