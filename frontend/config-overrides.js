9093cf840359187ea56b7af2b5330519f7d52c1d
Cory Waddingham <cory@waddingham.org>
2024-10-25 16:35:32 -0700

    Configure webpack to build separate bundles for each entry point Add load event listeners to ensure scripts are fully loaded before initialization Add more detailed logging to help diagnose any loading issues Use the correct bundle names in each template

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,23 +1,36 @@
+const path = require('path');
 const { override, addWebpackPlugin } = require('customize-cra');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-/*
-module.exports = override(
-  addWebpackPlugin(
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      filename: 'index.html',
-      chunks: ['sentimentChecker'],
-    })
-  ),
-  (config) => {
+module.exports = {
+  webpack: function(config, env) {
+    // Clear the existing entry point
+    config.entry = {};
+    
+    // Define new entry points
     config.entry = {
-      sentimentChecker: './src/index.tsx',
+      main: path.resolve(__dirname, 'src/indexMain.tsx'),
+      topbar: path.resolve(__dirname, 'src/indexTopbar.tsx'),
+      background: path.resolve(__dirname, 'src/indexBackground.tsx')
     };
-    config.output.filename = '[name].js';
-    config.devtool = 'source-map';
-    config.stats = 'verbose';
+
+    // Update output configuration
+    config.output = {
+      ...config.output,
+      filename: 'static/js/[name].bundle.js',
+      chunkFilename: 'static/js/[name].chunk.js',
+    };
+
     return config;
+  },
+  devServer: function(configFunction) {
+    return function(proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost);
+      config.static = {
+        ...config.static,
+        directory: path.join(__dirname, 'public'),
+      };
+      return config;
+    };
   }
-);
-*/
+};
