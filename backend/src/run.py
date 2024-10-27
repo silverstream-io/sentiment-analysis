cb717aae2182e3c06cf77c3ecfb9ddfd752d842b
Cory Waddingham <cory@waddingham.org>
2024-10-26 21:33:32 -0700

    Set WSGI to use Render default port and run.py to use 8080.

--- backend/src/run.py
+++ backend/src/run.py
@@ -2,4 +2,4 @@ from api.server import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- backend/src/wsgi.py
+++ backend/src/wsgi.py
@@ -1,6 +1,4 @@
-from api.server import create_app
-
-app = create_app()
+from api.server import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=10000)
