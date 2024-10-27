1f62482f3265588e2d73849e429d614cd96f8cf2
Cory Waddingham <cory@waddingham.org>
2024-10-26 22:10:13 -0700

    Moved instantiation of app in WSGI to top level.

--- backend/src/wsgi.py
+++ backend/src/wsgi.py
@@ -1,5 +1,12 @@
+import os
+import sys
+
+# Add the current directory to the path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from api.server import create_app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host='0.0.0.0', port=10000)
+    app.run()
